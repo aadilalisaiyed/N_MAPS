99a3b73c7ef6861c897b480441af3787ba1a13c7
NirmalyaASinha <n.nirmalya.n@gmail.com>
2026-02-07 12:23:42 +0530

    Delete ml directory

--- ml/preprocess.py
+++
@@ -1,9 +0,0 @@
-import re
-import emoji
-
-def clean_message_bert(text):
-    text = text.lower()
-    text = re.sub(r"http\S+|www\S+", "", text)
-    text = emoji.demojize(text)
-    text = text.replace(":", " ").replace("_", " ")
-    return re.sub(r"\s+", " ", text).strip()
